0f1b4dc66543308d851fe251998e690eb1ce7cf4
Nicolas <nicolascarrasco15@gmail.com>
2019-10-11 16:11:34 -0300

    modificacion de comenarios archivo conexion

--- conexion/conexion.php
+++ conexion/conexion.php
@@ -1,11 +1,11 @@
     
    <?php
-    // Forma de conexion PDO
+    // Conexion a la base de datos PDO
     $servidor = "mysql:dbname=empresa;host=127.0.0.1";
     $usuario="root";
     $password="";
 
-    //Si la conexion se realiza de forma correcta el try se ejecutara y me enviara el mensaje que esta dentro 
+    //Try catch, permite ejecutar el codigo, en caso de haber error o no, en el caso de haber, se ejecua el catch. 
     try{
         
         $pdo= new PDO($servidor, $usuario, $password);
@@ -14,6 +14,6 @@
     }catch(PDOException $e){
         
         echo "Conexion mala : ( ".$e->getMessage();
-        // en caso de haber problemas de todas formas se ejecutara, pero pasara al catch y nos mostrara el mensaje que esta dentro
     }
 ?>
+       
